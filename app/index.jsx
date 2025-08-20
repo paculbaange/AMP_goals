00723ab08e87ca80dcbc532d5d6baa6b9d08833d
paculbaange <warrendelfin09gmail.com>
2025-08-20 09:33:10 +0800

    index is changed

--- app/index.jsx
+++ app/index.jsx
@@ -5,7 +5,7 @@ const Home = () => {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>
-        A M P   G O A L S
+        A M P   G O A L S 
       </Text>
       <Link style={styles.link} href="/goals">
         View Your Goals
